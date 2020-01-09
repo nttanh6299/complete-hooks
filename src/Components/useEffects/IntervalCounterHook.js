a5a5ec70110ff51c76a4f29654f8135f34e57151
Anh Nguyen Tran <anh.nguyentran@exe.com.vn>
2020-01-09 14:52:05 +0700

    useEffect intermediate

--- src/Components/useEffects/IntervalCounterHook.js
+++ src/Components/useEffects/IntervalCounterHook.js
@@ -1,12 +1,28 @@
 import React, { useState, useEffect } from 'react';
 
+/*
+React only runs the effects after letting the browser paint.
+1.React renders UI for {id: 20}.
+2.The browser paints. We see the UI for {id: 20} on the screen.
+3.React cleans up the effect for {id: 10}.
+4.React runs the effect for {id: 20}.
+*/
+
 export const IntervalCounterHook = () => {
+    //going first after setting the count
     const [count, setCount] = useState(0);
 
     console.log("begin");
 
+    /*  
+    khi count = 0, tick sẽ chạy (ngữ cảnh hiện tại của tick là count = 0), tăng count
+    ở useEffect ko có dependencies nào => ignore useEffect (=> ko thể chuyển tick đến ngữ cảnh khác count != 0)
+    render (count + 1) = (0 + 1), sẽ mãi là 1
+    */
     const tick = () => {
-        setCount(prev => prev + 1);
+        console.log('set count');
+        setCount(count + 1)
+        console.log(count)
     }
 
     const doSomething = () => {
@@ -21,7 +37,7 @@ export const IntervalCounterHook = () => {
             console.log("Component unmounted");
             clearInterval(interval);
         }
-    }, []);
+    }, [count]);
 
     console.log("Render");
     return ( <div>
